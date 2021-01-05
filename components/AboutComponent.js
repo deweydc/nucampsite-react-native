ea3c44987e96ec8c348c63a5f8a4b4b48e1d3c5f
Dewey C <deweycraddock@gmail.com>
2021-01-04 20:19:26 -0600

    Icons , Favorites, and Comments

--- components/AboutComponent.js
+++ components/AboutComponent.js
@@ -7,12 +7,12 @@ import { PARTNERS } from '../shared/partners';
 function Mission() {
     return (
         <Card
-            title='Mission'
+            title='Our Mission'
             wrapperStyle={{ margin: 20 }}
         >
             <Text>
                 We present a curated database of the best campsites in the vast woods and backcountry of the World Wide Web Wilderness. We increase access to adventure for the public while promoting safe and respectful use of resources. The expert wilderness trekkers on our staff personally verify each campsite to make sure that they are up to our standards. We also present a platform for campers to share reviews on campsites they have visited with each other.
-        </Text>
+            </Text>
         </Card>
     )
 
