73ef69bb628288e626133e63ea7574a41691ef45
Dewey C <deweycraddock@gmail.com>
2021-01-14 19:45:24 -0600

    Alert

--- components/FavoritesComponent.js
+++ components/FavoritesComponent.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
-import { FlatList, View, Text, StyleSheet } from 'react-native';
-import { ListItem, ListView } from 'react-native-elements';
+import { FlatList, View, Text, StyleSheet, Alert } from 'react-native';
+import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
 import { Loading } from './LoadingComponent';
 import { baseUrl } from '../shared/baseUrl';
@@ -35,7 +35,27 @@ class Favorites extends Component {
                     <View style={styles.deleteView}>
                         <TouchableOpacity
                             style={styles.deleteTouchable}
-                            onPress={() => this.props.deleteFavorite(item.id)}
+                            onPress={() =>
+                                Alert.alert(
+                                    'Delete Favorite',
+                                    'Are you sure you want to delete the favorite campsite ' +
+                                    item.name +
+                                    '?',
+                                    [
+                                        {
+                                            text: 'Cancel',
+                                            onPress: () => console.log(item.name + 'Not Deleted'),
+                                            style: 'cancel'
+                                        },
+                                        {
+                                            text: 'OK',
+                                            onPress: () => this.props.deleteFavorite(item.id)
+                                        },
+
+                                    ],
+                                    { cancelable: false }
+                                )
+                            }
                         >
                             <Text style={styles.deleteText}>Delete</Text>
                         </TouchableOpacity>
