6068864d87cb3d123b7f9debebaa9639691cb234
Dewey C <deweycraddock@gmail.com>
2021-01-20 13:54:47 -0600

    Local Notifications

--- components/ReservationComponent.js
+++ components/ReservationComponent.js
@@ -5,6 +5,8 @@ import {
 } from 'react-native';
 import DateTimePicker from '@react-native-community/datetimepicker';
 import * as Animatable from 'react-native-animatable';
+import * as Notifications from 'expo-notifications';
+
 
 export default class Reservation extends Component {
 
@@ -41,6 +43,32 @@ export default class Reservation extends Component {
             showCalendar: false,
             showModal: false
         });
+    }
+
+    async presentLocalNotification(date) {
+        function sendNotification() {
+            Notifications.setNotificationHandler({
+                handleNotification: async () => ({
+                    shouldShowAlert: true
+                })
+            });
+
+            Notifications.scheduleNotificationAsync({
+                content: {
+                    title: 'Your Campsite Reservation Search',
+                    body: `Search for ${date} requested`
+                },
+                trigger: null
+            });
+        }
+
+        let permissions = await Notifications.getPermissionsAsync();
+        if (!permissions.granted) {
+            permissions = await Notifications.requestPermissionsAsync();
+        }
+        if (permissions.granted) {
+            sendNotification();
+        }
     }
 
     render() {
@@ -110,7 +138,10 @@ export default class Reservation extends Component {
                                     },
                                     {
                                         text: 'OK',
-                                        onPress: () => { this.resetForm() }
+                                        onPress: () => {
+                                            this.presentLocalNotification(this.state.date.toLocaleDateString('en-US'));
+                                            this.resetForm()
+                                        }
                                     },
 
                                 ],
